45a65b4735ffa24fa01edd7c4c736aabc89951c5
ArshjotKaur02 <arshjot.kaur@ucalgary.ca>
2026-03-23 22:44:11 -0600

    Implement final knn classifier with k = 3 for optimal performance

--- KNeighborsClassifier_Model.py
+++ KNeighborsClassifier_Model.py
@@ -3,7 +3,6 @@
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
-import matplotlib.pyplot as plt
 
 #Importing processed data from Stroke_Data_Processing.py
 from Stroke_Data_Processing import feature_train, feature_test, target_train, target_test
@@ -15,24 +14,33 @@ from Stroke_Data_Processing import feature_train, feature_test, target_train, ta
 # comparable scale around 0.
 
 scaler = StandardScaler()
-X_train_scaled = scaler.fit_transform(feature_train)  #Learn mean/std from train set and then scale it
+feature_train_scaled = scaler.fit_transform(feature_train)  #Learn mean/std from train set and then scale it
+feature_test_scaled  = scaler.transform(feature_test)
 
-#Testing various k values for maximum performance
-accuracies = []
-k_values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+# --------------------------------------------- TRAIN THE KNN MODEL ----------------------------------------------------
 
-for k in k_values:
-    knn = KNeighborsClassifier(n_neighbors=k, metric="euclidean")
-    knn.fit(feature_train, target_train)
-    predicted = knn.predict(feature_test)
+# Choosing k = 3 for a balance of accuracy, f1-score and recall
+# Using metric = Euclidean to measure straight-line distance
+kNeighbour_model = KNeighborsClassifier(n_neighbors = 3, metric = "euclidean")
+kNeighbour_model.fit(feature_train_scaled, target_train)
+predicted_labels = kNeighbour_model.predict(feature_test_scaled)
 
-    accuracy = accuracy_score(target_test, predicted)
-    accuracies.append(accuracy)
+# --------------------------------------------- EVALUATION METRICS -----------------------------------------------------
 
-print(accuracies)
+kmm_accuracy_score = accuracy_score(target_test, predicted_labels)
+knn_precision = precision_score(target_test, predicted_labels)
+knn_recall = recall_score(target_test, predicted_labels)
+knn_f1_score = f1_score(target_test, predicted_labels)
+knn_confusion_matrix = confusion_matrix(target_test, predicted_labels)
 
-plt.plot(k_values, accuracies, '-o', label='Accuracy Score', color='orange')
-plt.xlabel("K-Value")
-plt.ylabel("Accuracy Score")
-plt.legend()
-plt.show()
+print("-" * 45) # Visual seperator
+print("-*- KNN PERFORMANCE METRICS -*-")
+print("-" * 45)
+
+print(f"Accuracy  : {kmm_accuracy_score:.4f}")
+print(f"Precision : {knn_precision:.4f}")
+print(f"Recall    : {knn_recall:.4f}")
+print(f"F1-Score  : {knn_f1_score:.4f}")
+
+print("\nConfusion Matrix:")
+print(knn_confusion_matrix)
