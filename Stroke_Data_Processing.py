1465b3fa753a6b07f6b43b97c57f763dbcd94988
ArshjotKaur02 <arshjot.kaur@ucalgary.ca>
2026-03-23 21:30:43 -0600

    Encode categorical variables and split data into training and testing sets for fair comparison

--- Stroke_Data_Processing.py
+++ Stroke_Data_Processing.py
@@ -46,4 +46,34 @@ median_bmi_value = patient_records_dataframe["bmi"].median()
 # Using fillna() to replaces all NaN values with the median BMI value
 patient_records_dataframe["bmi"] = patient_records_dataframe["bmi"].fillna(median_bmi_value)
 
+# ---------------------------------------- HANDLING CATEGORICAL DATA ---------------------------------------------------
 
+# Using Binary encoding for Yes/No columns
+# "ever_married" has two values. We'll use 1 for Yes and 0 for No
+patient_records_dataframe["ever_married"] = patient_records_dataframe["ever_married"].map({"Yes": 1, "No": 0})
+
+# "Residence_type" has two values. We'll use 1 for Urban and 0 for Rural
+patient_records_dataframe["Residence_type"] = patient_records_dataframe["Residence_type"].map({"Urban": 1, "Rural": 0})
+
+# For columns with multiple categories, we'll use One-Hot Encoding. This is because using just 1, 2, 3 might be misleading
+# as the model may consider it as numbers with numeric values such as 3 is greater than 1, which is incorrect.
+
+# Information on .get_dummies() was accessed from: https://www.geeksforgeeks.org/pandas/python-pandas-get_dummies-method/
+# This creates a separate binary column for each category except for first category (drop_first = true).
+patient_records_dataframe = pd.get_dummies(patient_records_dataframe,
+                                           columns=["gender", "work_type", "smoking_status"], drop_first=True)
+
+# -------------------------------------- SEPARATING FEATURE MATRIX -----------------------------------------------------
+
+feature_matrix = patient_records_dataframe.drop(columns=["stroke"]) #All 11 feature columns except stroke
+target_label = patient_records_dataframe["stroke"]  #Only the stroke column
+
+# -------------------------------------- SPLITTING TRAIN TEST DATA -----------------------------------------------------
+#
+# We'll use 70/30 split, where:
+# test_size = 0.3 to separate 30% of data as test set and 70% as train set
+# random_state = 42 to set the seed for the random number generator. This ensures the split is reproducible
+# stratify = target_label to ensure that the stroke proportion is the same in both train and test sets as the imbalanced
+
+feature_train, feature_test, target_train, target_test = train_test_split(feature_matrix, target_label,
+                                                            test_size = 0.3, random_state = 42, stratify = target_label)
